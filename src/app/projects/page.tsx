61c53f9754576a9d8484445aedec938ce71ac6f1
AmirtterK <strikereureka7661@gmail.com>
2025-08-22 00:46:46 +0100

    first steps

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ import Projects from "@/components/Projects";
 export default function Home() {
   return (
     <section className="flex flex-col gap-8">
-      <h1 className={`${inter.className} font-light text-foreground text-5xl tracking-wide`}>My Projects</h1>
+      <h1 className={`${inter.className} font-light text-foreground text-5xl tracking-wide`}>My projects</h1>
 
     <Projects/>
     </section>
