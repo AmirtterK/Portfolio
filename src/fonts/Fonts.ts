7bfd315d5ae847b804aece2e1c5de4497f9dd3e7
AmirtterK <strikereureka7661@gmail.com>
2025-08-21 22:30:03 +0100

    first dep

--- src/fonts/Fonts.ts
+++ src/fonts/Fonts.ts
@@ -1,11 +1,11 @@
 import localFont from "next/font/local";
 
 export const inter = localFont({
-  src: "interVariable.woff2", 
+  src: "./interVariable.woff2", 
   variable: "--font-myfont",   
 });
 
 export const avant = localFont({
-  src: "avantgarde.woff2", 
+  src: "./avantgarde.woff2", 
   variable: "--font-myfont",   
 }); 
