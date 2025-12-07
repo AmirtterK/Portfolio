942eaca96d7936a71622ea7d62b7b49e41c3e2c2
AmirtterK <strikereureka7661@gmail.com>
2025-12-08 00:12:45 +0100

    lint

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   // eslint: {
-  //   ignoreDuringBuilds: true,
+  //   ignoreDuringBuilds: true, 
   // },
 };
 
